fba8b0dd548b761686c737315a931ea72e8bd4d3
Cedric Skwar <cdrc@5y5.one>
2023-11-04 17:00:26 +0100

    comment

--- java/src/main/java/xyz/skwar/sorting/BogoSort.java
+++ java/src/main/java/xyz/skwar/sorting/BogoSort.java
@@ -2,6 +2,13 @@ package xyz.skwar.sorting;
 
 import java.util.*;
 
+/*
+ * BogoSort Implementation
+ * Note: This sorting algorithm is included for educational purposes only.
+ * It is highly inefficient and not recommended for actual sorting tasks.
+ * The algorithm sorts the list purely by chance by repeatedly shuffling
+ * the entire list until it happens to be in order.
+ */
 public class BogoSort {
   private ArrayList<Integer> randomIntList;
 
