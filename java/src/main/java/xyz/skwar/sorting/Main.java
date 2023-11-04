64ddbaeed2d92e3f434636a4f354ba8c11dda42b
Cedric Skwar <cdrc@5y5.one>
2023-11-04 15:43:19 +0100

    meassure time and log it

--- java/src/main/java/xyz/skwar/sorting/Main.java
+++ java/src/main/java/xyz/skwar/sorting/Main.java
@@ -2,24 +2,35 @@ package xyz.skwar.sorting;
 
 import java.util.ArrayList;
 import java.util.Random;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Main {
+  private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
   public static void main(String[] args) {
     // Create an ArrayList and populate it with random values
     ArrayList<Integer> randomIntList = new ArrayList<>();
     Random rand = new Random();
+    int listLength = 1000;
 
-    for (int i = 0; i < 200; i++) {
-      randomIntList.add(rand.nextInt(1000));
+    for (int i = 0; i < listLength; i++) {
+      randomIntList.add(rand.nextInt(listLength * 5));
     }
 
-    // Create an instance of the Sorting class
     BubbleSort bubbleSort = new BubbleSort(randomIntList);
 
-    // Call the BubbleSort method and store the sorted list
-    ArrayList<Integer> bubbleSortedList = bubbleSort.bubbleSort();
+    // Time bubbleSort
+    long bubbleSortTime = measureTime(bubbleSort::bubbleSort);
 
-    // Print the sorted list
-    System.out.println("Sorted List: " + bubbleSortedList);
+    // Print time duration for each sorter
+    logger.info("Time taken by BubbleSort: {} ms", bubbleSortTime);
+  }
+
+  private static long measureTime(Runnable action) {
+    long startTimeBubble = System.currentTimeMillis();
+    action.run();
+    long endTimeBubble = System.currentTimeMillis();
+    return endTimeBubble - startTimeBubble;
   }
 }
